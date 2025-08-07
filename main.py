82bd9a6d8c6d0ed46d1245d40941bfc43a645ae1
Raquel Torres <raquelptotazu@gmail.com>
2025-08-07 11:20:06 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+//Quiero un programa que pueda leer el excel de la facultad y decirme qué horario y en qué aula hay x clase
+//Adicionalmente, podré ver las aulas vacías en un horario especificado
+
+
+print("Bienvenido al programa de aulas!")
+print("Qué desea hacer?")
+print("1-Ver horario y aula.\n2-Ver aulas vacías")
