a9ebe72bfb53afc6c6c719e717fd6da41e1b22fa
Raquel Torres <raquelptotazu@gmail.com>
2025-08-07 11:26:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 //Quiero un programa que pueda leer el excel de la facultad y decirme qué horario y en qué aula hay x clase
 //Adicionalmente, podré ver las aulas vacías en un horario especificado
 
+//TODO: Abrir el archivo excel al empezar el programa
 
-print("Bienvenido al programa de aulas!")
-print("Qué desea hacer?")
-print("1-Ver horario y aula.\n2-Ver aulas vacías")
+while (true):
+    print("Bienvenido al programa de aulas!")
+    print("Qué desea hacer?")
+    print("1-Ver horario y aula.\n2-Ver aulas vacías.\n3-Salir")
+    accion = input()
+    if(accion < 1 or accion >3):
+        print("Esa no es una opción válida.")
+
+    if(accion == 3):
+      break
+
+    if(accion == 1):
+      //encontrar_aulas()
+
+    if(accion == 2):
+      //encontrar_vacias()
+    
